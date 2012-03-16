181f4abff66ebdd7463ad4cc51c94af433413729
jushi <jushi@taobao.com>
2012-03-16 15:50:24 +0800

    Introduce FileInputStream cache.

--- src/main/java/com/github/zhongl/io/FileChannels.java
+++ src/main/java/com/github/zhongl/io/FileChannels.java
@@ -17,6 +17,11 @@
 package com.github.zhongl.io;
 
 import com.google.common.base.Function;
+import com.google.common.base.Throwables;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.RemovalListener;
+import com.google.common.cache.RemovalNotification;
 import com.google.common.io.Closeables;
 
 import java.io.File;
@@ -24,25 +29,63 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /** @author <a href="mailto:zhong.lunfu@gmail.com">zhongl<a> */
 public class FileChannels {
 
+    private static final Cache<ThreadBound, FileInputStream> CACHE;
+    public static final Callable<FileInputStream> VALUE_LOADER = new Callable<FileInputStream>() {
+        @Override
+        public FileInputStream call() throws Exception {
+            throw new UnsupportedOperationException();
+        }
+    };
+
+    static {
+        CACHE = CacheBuilder.newBuilder()
+                            .concurrencyLevel(Runtime.getRuntime().availableProcessors())
+                            .expireAfterAccess(1, TimeUnit.SECONDS)
+                            .removalListener(new RemovalListener<ThreadBound, FileInputStream>() {
+                                @Override
+                                public void onRemoval(RemovalNotification<ThreadBound, FileInputStream> notification) {
+                                    Closeables.closeQuietly(notification.getValue());
+                                }
+                            })
+                            .build();
+    }
+
     private FileChannels() {}
 
-    public static <T> T read(File file, FileChannelFunction<T> function) throws IOException {
-        return read(new FileInputStream(file), function);
+    public static <T> T read(final File file, FileChannelFunction<T> function) throws IOException {
+        try {
+            return function.apply(CACHE.get(new ThreadBound(file), new FileInputStreamLoader(file)).getChannel());
+        } catch (ExecutionException e) {
+            Throwable cause = e.getCause();
+            Throwables.propagateIfInstanceOf(cause, IOException.class);
+            throw new IllegalStateException(cause);
+        }
     }
 
     public static <T> T read(File file, final long position, final int length, final Function<ByteBuffer, T> function) throws IOException {
         final ByteBuffer byteBuffer = ByteBuffer.allocate(length);
-        return read(new FileInputStream(file), new FileChannelFunction<T>() {
-            @Override
-            public T apply(FileChannel channel) throws IOException {
-                read(channel, position, byteBuffer);
-                return function.apply(byteBuffer);
-            }
-        });
+        try {
+            return new FileChannelFunction<T>() {
+                @Override
+                public T apply(FileChannel channel) throws IOException {
+                    read(channel, position, byteBuffer);
+                    return function.apply(byteBuffer);
+                }
+            }.apply(CACHE.get(new ThreadBound(file), new FileInputStreamLoader(file)).getChannel());
+        } catch (ExecutionException e) {
+            Throwable cause = e.getCause();
+            Throwables.propagateIfInstanceOf(cause, IOException.class);
+            throw new IllegalStateException(cause);
+        }
     }
 
     public static void read(FileChannel channel, long position, ByteBuffer byteBuffer) throws IOException {
@@ -53,24 +96,43 @@ public class FileChannels {
         byteBuffer.flip();
     }
 
-    private static <T> T read(FileInputStream stream, FileChannelFunction<T> function) throws IOException {
-        try {
-            return read(stream.getChannel(), function);
-        } finally {
-            Closeables.closeQuietly(stream);
-        }
-    }
-
-    private static <T> T read(FileChannel channel, FileChannelFunction<T> function) throws IOException {
-        try {
-            return function.apply(channel);
-        } finally {
-            Closeables.closeQuietly(channel);
-        }
-    }
-
     public static interface FileChannelFunction<T> {
         public T apply(FileChannel channel) throws IOException;
     }
 
+    private static class ThreadBound {
+        private final File file;
+        private final Thread current;
+
+        private ThreadBound(File file) {
+            this.file = checkNotNull(file);
+            current = Thread.currentThread();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ThreadBound that = (ThreadBound) o;
+            return current.equals(that.current) && file.equals(that.file);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = file.hashCode();
+            result = 31 * result + current.hashCode();
+            return result;
+        }
+    }
+
+    private static class FileInputStreamLoader implements Callable<FileInputStream> {
+        private final File file;
+
+        public FileInputStreamLoader(File file) {this.file = file;}
+
+        @Override
+        public FileInputStream call() throws Exception {
+            return new FileInputStream(file);
+        }
+    }
 }
