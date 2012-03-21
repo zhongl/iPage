e4fcc8ac461ba5a5a44a8a32b18ae8f22927f5b4
jushi <jushi@taobao.com>
2012-03-21 11:04:39 +0800

    Verify #51.

--- src/test/java/com/github/zhongl/api/EphemeronsTest.java
+++ src/test/java/com/github/zhongl/api/EphemeronsTest.java
@@ -25,8 +25,10 @@ import com.google.common.util.concurrent.FutureCallback;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
@@ -150,12 +152,34 @@ public class EphemeronsTest {
         for (int i = 0; i < 8; i++) ephemerons.add(key(i), i, ignore);
 
         afterMerging();
+        assertThat(store.ordering.size(), greaterThanOrEqualTo(4));
         for (int i = 0; i < store.ordering.size(); i++) assertThat(store.ordering.get(i), is(i));
 
         ephemerons.flush(); // merge rest 4
 
         afterMerging();
         for (int i = 0; i < store.ordering.size(); i++) assertThat(store.ordering.get(i), is(i));
+
+        assertThat(ephemerons.throughout(0), is(4));
+    }
+
+    @Test
+    public void issue51() throws Exception {
+        // Fixed #51 : Unexpected thoughput increment.
+        ephemerons.throughout(4);
+
+        store.setNextMerge(true);
+        for (int i = 0; i < 8; i++) ephemerons.add(key(i), i, ignore);
+
+        afterMerging();
+
+        store.setNextMerge(true);
+        ephemerons.flush(); // merge rest 4
+
+        afterMerging();
+
+        assertThat(ephemerons.throughout(0), is(4));
+
     }
 
     private Key key(int i) {return new Md5Key(Md5.md5((i + "").getBytes()));}
@@ -164,20 +188,36 @@ public class EphemeronsTest {
         final Map<Key, Integer> appendings = Collections.synchronizedMap(new HashMap<Key, Integer>());
         final Set<Key> removings = Collections.synchronizedSet(new HashSet<Key>());
         final List<Integer> ordering = Collections.synchronizedList(new ArrayList<Integer>());
+        private boolean failed;
 
         public Integer get(Key key) {
             if (removings.contains(key)) return null;
             return appendings.get(key);
         }
 
+        public void setNextMerge(boolean failed) {
+            this.failed = failed;
+        }
+
         public void merge(Collection<WriteOperation<Entry<Key, Integer>>> addOrUpdates,
                           Collection<WriteOperation<Key>> removes, FutureCallback<Void> futureCallback) {
-            try { Thread.sleep(10L); } catch (InterruptedException e) { e.printStackTrace(); }
+            try { TimeUnit.MILLISECONDS.sleep(10L); } catch (InterruptedException e) { e.printStackTrace(); }
+
+            if (failed) {
+                onFailure(addOrUpdates);
+                onFailure(removes);
+                failed = true;
+                futureCallback.onSuccess(Nils.VOID);
+                System.out.println("failed");
+                return;
+            }
+
             for (WriteOperation<Entry<Key, Integer>> operation : addOrUpdates) {
                 Entry<Key, Integer> entry = operation.attachement();
                 this.appendings.put(entry.key(), entry.value());
                 ordering.add(entry.value());
             }
+
             for (WriteOperation<Key> operation : removes) this.removings.add(operation.attachement());
             onSuccess(addOrUpdates);
             onSuccess(removes);
@@ -186,6 +226,10 @@ public class EphemeronsTest {
 
         private void onSuccess(Collection<? extends FutureCallback<Void>> callbacks) {
             for (FutureCallback<Void> callback : callbacks) callback.onSuccess(null);
+        }
+
+        private void onFailure(Collection<? extends FutureCallback<Void>> callbacks) {
+            for (FutureCallback<Void> callback : callbacks) callback.onFailure(new IOException());
         }
     }
 
